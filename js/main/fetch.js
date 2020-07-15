a8ab8199cf44ae70166b7085c92cb87a794d9e7d
Gergez <53828655+Gergez@users.noreply.github.com>
2020-07-15 11:14:22 +0200

    fetch műkszik

--- js/main/fetch.js
+++ js/main/fetch.js
@@ -2,11 +2,11 @@ function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
+      document.getElementById("navbar").innerHTML =
       this.responseText;
     }
   };
-  xhttp.open("GET", "asdasdsa", true);
+  xhttp.open("GET", "../js/fetch/navbar.html", true);
   xhttp.send();
 }
 

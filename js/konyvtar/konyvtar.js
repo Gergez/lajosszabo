c7f44ebc4a1e2632eeed94aee34c519b39dc179f
Gergez <53828655+Gergez@users.noreply.github.com>
2020-08-16 21:27:07 +0200

    konyvtár

---
+++ js/konyvtar/konyvtar.js
@@ -0,0 +1,15 @@
+let inputs = document.querySelectorAll('form input');
+let labels = document.querySelectorAll('form label');
+
+// for(let i = 0; i < inputs.length; i++){
+//   inputs[i].addEventListener('click', () => {
+//     inputs[i].setAttribute('checked', true);
+//   });
+// }
+
+
+// for(let i = 0; i < inputs.length; i++){
+//   if(inputs[i].hasAttribute('checked')){
+//     console.log("OKAY THIS IS CHECKED" + inputs[i]);
+//   }
+// }
